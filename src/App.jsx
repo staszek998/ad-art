7a77cb61d882f104cf4da4c5974e404f1fe709d8
Stanisław Gregor <gregor.stanislaw@hotmail.com>
2019-02-03 11:49:16 +0100

    Remove the disposable code

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
 // JS imports
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 // SCSS imports
 import "./App.scss";
 
-const Home = () => <div>Home</div>;
-
-const About = () => <div>About</div>;
-
-const Topics = () => <div>Topics</div>;
-
 const App = () => (
   <div className="App">
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
-      </div>
+      <div>This is being rendered by the react-router.</div>
     </Router>
   </div>
 );
