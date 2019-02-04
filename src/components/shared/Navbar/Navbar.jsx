8df9f07f4a8f9aeda37168291d43728aa7abc8e0
Stanisław Gregor <stanislaw.gregor@plej.pl>
2019-02-04 11:48:42 +0100

    Change the HTML markup of the Navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -2,22 +2,49 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => (
-    <header class="Navbar">
+    <nav class="Navbar navbar navbar-expand-md">
         <div className="container">
-            <div className="row">
-                <div className="col-12 d-flex justify-content-between align-items-center">
-                    <NavLink to="/">AD-art</NavLink>
-                    <nav class="d-flex justify-content-center align-items-between">
+            {/* Company name */}
+            <NavLink to="/">AD-art</NavLink>
+
+            {/* Dropdown toggle button */}
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbar-collapse"
+                aria-controls="navbar-collapse"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon" />
+            </button>
+
+            {/* Navbar links */}
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
                         <NavLink to="/o-nas">O NAS</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/o-nas">O NAS</NavLink>
+                    </li>
+                    <li className="nav-item">
                         <NavLink to="/realizacje">REALIZACJE</NavLink>
+                    </li>
+                    <li className="nav-item">
                         <NavLink to="/projekty">PROJEKTY</NavLink>
+                    </li>
+                    <li className="nav-item">
                         <NavLink to="/oferta">OFERTA</NavLink>
+                    </li>
+                    <li className="nav-item">
                         <NavLink to="/kontakt">KONTAKT</NavLink>
-                    </nav>
-                </div>
+                    </li>
+                </ul>
             </div>
         </div>
-    </header>
+    </nav>
 );
 
 export default Navbar;
