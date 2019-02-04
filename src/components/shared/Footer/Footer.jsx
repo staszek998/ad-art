1f029d30fdfcb29f3f32f460a2056845d3275fe7
Stanisław Gregor <stanislaw.gregor@plej.pl>
2019-02-04 09:05:39 +0100

    Create the Footer component

---
+++ src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+    <footer className="Footer">
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center">
+                    AD-art 2019 &copy; Wszelkie prawa zastrzeżone
+                </div>
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer;
