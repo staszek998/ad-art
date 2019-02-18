6ab29d16154308fe07838fe0dafe820d1c87b34e
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-18 09:47:14 +0100

    Create basic JSX elements layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-const Home = () => <main className="Home">Home</main>;
+const Home = () => (
+  <main className="Home">
+    {/* Heading / intro */}
+    <h1>AD-art</h1>
+    <h2>Projektujemy Twoją przestrzeń</h2>
+
+    {/* Links */}
+    <div className="links">
+      <NavLink to="/relizacje">Zobacz nasze realizacje</NavLink>
+      <NavLink to="/kontakt">Skontaktuj się z nami</NavLink>
+    </div>
+  </main>
+);
 
 export default Home;
