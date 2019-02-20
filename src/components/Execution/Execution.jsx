f31f597aca69e6b6134415aeaa78941f0cbe7faa
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-20 11:31:11 +0100

    Start implementing the Codrops' CSS3 Lightbox

--- src/components/Execution/Execution.jsx
+++ src/components/Execution/Execution.jsx
@@ -1,15 +1,60 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./_Execution.scss";
 
-const Execution = () => (
+const Execution = ({ photos, title }) => (
   <main className="Execution">
     <div className="container">
-      <div className="grid-container">
-        <div className="grid-item">grid-item</div>
+      <div className="row">
+        <div className="col">
+          {/* Execution title */}
+          <h1 className="display-1">{title}</h1>
+
+          {/* Photos grid */}
+          <ul class="lb-album">
+            {photos.map((photo, index) => {
+              <li>
+                <a href={`#image-${index}`}>
+                  <img src={photo.thumbnail} alt="" />
+                  <span>{photo.title}</span>
+                </a>
+                <div class="lb-overlay" id={`image-${index}`}>
+                  <img src={photo.full} alt="" />
+                  <div>
+                    <h3>{photo.title}</h3>
+                    <p>{photo.description}</p>
+                    <a
+                      href={`#image-${
+                        index === 0 ? photos.length + 1 : index - 1
+                      }`}
+                      class="lb-prev"
+                    >
+                      Poprzedni
+                    </a>
+                    <a
+                      href={`#image-${index > photos.length ? 0 : index + 1}`}
+                      class="lb-next"
+                    >
+                      Następny
+                    </a>
+                  </div>
+                  <a href="#page" class="lb-close">
+                    X ZAMKNIJ
+                  </a>
+                </div>
+              </li>;
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   </main>
 );
+
+Executions.propTypes = {
+  photos: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string
+};
 
 export default Execution;
