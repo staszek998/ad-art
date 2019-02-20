5fe3fe6a3c0c1820dc81d1df09e4f5462ba149b2
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-20 11:44:02 +0100

    Create the JS file with the example execution contents

---
+++ src/db/example-execution.js
@@ -0,0 +1,47 @@
+const exampleExecution = {
+  title: "Dom",
+  photos: [
+    {
+      title: "Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus distinctio, numquam doloremque quas consectetur accusantium soluta modi esse corporis assumenda, molestiae ut. Omnis placeat praesentium asperiores doloremque sit quo commodi?",
+      thumbnail: "https://dummyimage.com/600x400/000/fff&text=Thumbnail",
+      full: "https://dummyimage.com/600x400/000/fff&text=FULL"
+    },
+    {
+      title: "Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus distinctio, numquam doloremque quas consectetur accusantium soluta modi esse corporis assumenda, molestiae ut. Omnis placeat praesentium asperiores doloremque sit quo commodi?",
+      thumbnail: "https://dummyimage.com/600x400/000/fff&text=Thumbnail",
+      full: "https://dummyimage.com/600x400/000/fff&text=FULL"
+    },
+    {
+      title: "Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus distinctio, numquam doloremque quas consectetur accusantium soluta modi esse corporis assumenda, molestiae ut. Omnis placeat praesentium asperiores doloremque sit quo commodi?",
+      thumbnail: "https://dummyimage.com/600x400/000/fff&text=Thumbnail",
+      full: "https://dummyimage.com/600x400/000/fff&text=FULL"
+    },
+    {
+      title: "Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus distinctio, numquam doloremque quas consectetur accusantium soluta modi esse corporis assumenda, molestiae ut. Omnis placeat praesentium asperiores doloremque sit quo commodi?",
+      thumbnail: "https://dummyimage.com/600x400/000/fff&text=Thumbnail",
+      full: "https://dummyimage.com/600x400/000/fff&text=FULL"
+    },
+    {
+      title: "Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus distinctio, numquam doloremque quas consectetur accusantium soluta modi esse corporis assumenda, molestiae ut. Omnis placeat praesentium asperiores doloremque sit quo commodi?",
+      thumbnail: "https://dummyimage.com/600x400/000/fff&text=Thumbnail",
+      full: "https://dummyimage.com/600x400/000/fff&text=FULL"
+    },
+    {
+      title: "Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus distinctio, numquam doloremque quas consectetur accusantium soluta modi esse corporis assumenda, molestiae ut. Omnis placeat praesentium asperiores doloremque sit quo commodi?",
+      thumbnail: "https://dummyimage.com/600x400/000/fff&text=Thumbnail",
+      full: "https://dummyimage.com/600x400/000/fff&text=FULL"
+    }
+  ]
+};
