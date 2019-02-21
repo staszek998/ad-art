86280154b9e1fd50aa7974e3bf0e82e82c00a1aa
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-21 12:27:07 +0100

    Remove the disposable CSS prefixes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,32 +3,32 @@ import React from "react";
 import "./_Gallery.scss";
 
 const Gallery = ({ photos }) => (
-  <ul class="Gallery lb-album">
+  <ul className="Gallery lb-album">
     {photos.map((photo, index) => (
-      <li>
+      <li key={`image-${index}`}>
         <a href={`#image-${index}`}>
           <img src={photo.src.thumb} alt={photo.alt} />
           <span>{photo.title}</span>
         </a>
-        <div class="lb-overlay" id={`image-${index}`}>
+        <div className="lb-overlay" id={`image-${index}`}>
           <img src={photo.src.full} alt={photo.alt} />
           <div>
             <h3>{photo.title}</h3>
             <p>{photo.description}</p>
             <a
               href={`#image-${index === 0 ? photos.length - 1 : index - 1}`}
-              class="lb-prev"
+              className="lb-prev"
             >
               Poprzednie
             </a>
             <a
               href={`#image-${index === photos.length - 1 ? 0 : index + 1}`}
-              class="lb-next"
+              className="lb-next"
             >
               Następne
             </a>
           </div>
-          <a href="#page" class="lb-close">
+          <a href="#page" className="lb-close">
             x Zamknij
           </a>
         </div>
