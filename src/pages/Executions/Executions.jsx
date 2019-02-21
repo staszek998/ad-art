3597e32e237f9ab5f71c17bf7a4ba2ffb4f4053a
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-21 15:49:49 +0100

    Make the grid items perfect squares

--- src/pages/Executions/Executions.jsx
+++ src/pages/Executions/Executions.jsx
@@ -15,35 +15,35 @@ const Executions = () => (
 
           {/* Cards */}
           <div className="grid-container">
-            <div className="grid-item--top-left">
+            <div className="grid-item grid-item--top-left">
               <Thumbnail
                 imgSrc="https://images.unsplash.com/photo-1495433324511-bf8e92934d90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
                 title="Dom"
                 href="/realizacje/dom"
               />
             </div>
-            <div className="grid-item--bottom-left">
+            <div className="grid-item grid-item--bottom-left">
               <Thumbnail
                 imgSrc="https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
                 title="Mieszkanie"
                 href="/realizacje/dom"
               />
             </div>
-            <div className="grid-item--center">
+            <div className="grid-item grid-item--center">
               <Thumbnail
                 imgSrc="https://images.unsplash.com/photo-1502005229762-cf1b2da7c5d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
                 title="Apartament"
                 href="/realizacje/dom"
               />
             </div>
-            <div className="grid-item--top-right">
+            <div className="grid-item grid-item--top-right">
               <Thumbnail
                 imgSrc="https://images.unsplash.com/photo-1542290504-2dc9a5811cd4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
                 title="Mieszkanie"
                 href="/realizacje/dom"
               />
             </div>
-            <div className="grid-item--bottom-right">
+            <div className="grid-item grid-item--bottom-right">
               <Thumbnail
                 imgSrc="https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&auto=format&fit=crop&w=1506&q=80"
                 title="Dom"
