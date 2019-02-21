8aac37e01401702114f2be3e477d33f1b0ea9f69
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-21 14:41:20 +0100

    Hide the Thumbnail's title when not hovered

--- src/pages/Executions/Executions.jsx
+++ src/pages/Executions/Executions.jsx
@@ -3,19 +3,28 @@ import React from "react";
 import "./_Executions.scss";
 import Thumbnail from "../../components/Thumbnail/Thumbnail";
 
+import exampleImage1 from "../../assets/images/executions/example-images/1.jpg";
+import exampleImage2 from "../../assets/images/executions/example-images/2.jpg";
+import exampleImage3 from "../../assets/images/executions/example-images/3.jpg";
+import exampleImage4 from "../../assets/images/executions/example-images/4.jpg";
+import exampleImage5 from "../../assets/images/executions/example-images/5.jpg";
+import exampleImage6 from "../../assets/images/executions/example-images/6.jpg";
+
 const Executions = () => (
   <main className="Executions">
     <div className="container">
       <div className="row">
         <div className="col-12">
           {/* Heading */}
-          <h1 className="display-2 display-md-1 mb-5 text-center">Realizacje</h1>
+          <h1 className="display-2 display-md-1 mb-5 text-center">
+            Realizacje
+          </h1>
 
           {/* Cards */}
           <div className="grid-container">
             <div className="grid-item--top-left">
               <Thumbnail
-                imgSrc="https://dummyimage.com/600x400/000/fff"
+                imgSrc={exampleImage1}
                 title="Some title"
                 subtitle="Some subtitle"
                 href="/realizacje/dom"
@@ -23,7 +32,7 @@ const Executions = () => (
             </div>
             <div className="grid-item--bottom-left">
               <Thumbnail
-                imgSrc="https://dummyimage.com/600x400/000/fff"
+                imgSrc={exampleImage2}
                 title="Some title"
                 subtitle="Some subtitle"
                 href="/realizacje/dom"
@@ -31,7 +40,7 @@ const Executions = () => (
             </div>
             <div className="grid-item--center">
               <Thumbnail
-                imgSrc="https://dummyimage.com/600x400/000/fff"
+                imgSrc={exampleImage3}
                 title="Some title"
                 subtitle="Some subtitle"
                 href="/realizacje/dom"
@@ -39,7 +48,7 @@ const Executions = () => (
             </div>
             <div className="grid-item--top-right">
               <Thumbnail
-                imgSrc="https://dummyimage.com/600x400/000/fff"
+                imgSrc={exampleImage4}
                 title="Some title"
                 subtitle="Some subtitle"
                 href="/realizacje/dom"
@@ -47,7 +56,7 @@ const Executions = () => (
             </div>
             <div className="grid-item--bottom-right">
               <Thumbnail
-                imgSrc="https://dummyimage.com/600x400/000/fff"
+                imgSrc={exampleImage5}
                 title="Some title"
                 subtitle="Some subtitle"
                 href="/realizacje/dom"
