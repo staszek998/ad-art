5414fae6cd95c487dda16bc3b063823b8916793b
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-21 15:00:15 +0100

    Remove the downloaded images. Use the hosted ones.

--- src/pages/Executions/Executions.jsx
+++ src/pages/Executions/Executions.jsx
@@ -3,13 +3,6 @@ import React from "react";
 import "./_Executions.scss";
 import Thumbnail from "../../components/Thumbnail/Thumbnail";
 
-import exampleImage1 from "../../assets/images/executions/example-images/1.jpg";
-import exampleImage2 from "../../assets/images/executions/example-images/2.jpg";
-import exampleImage3 from "../../assets/images/executions/example-images/3.jpg";
-import exampleImage4 from "../../assets/images/executions/example-images/4.jpg";
-import exampleImage5 from "../../assets/images/executions/example-images/5.jpg";
-import exampleImage6 from "../../assets/images/executions/example-images/6.jpg";
-
 const Executions = () => (
   <main className="Executions">
     <div className="container">
@@ -24,41 +17,41 @@ const Executions = () => (
           <div className="grid-container">
             <div className="grid-item--top-left">
               <Thumbnail
-                imgSrc={exampleImage1}
-                title="Some title"
-                subtitle="Some subtitle"
+                imgSrc="https://images.unsplash.com/photo-1495433324511-bf8e92934d90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+                title="Dom"
+                subtitle="Błękit Morza Bałtyckiego"
                 href="/realizacje/dom"
               />
             </div>
             <div className="grid-item--bottom-left">
               <Thumbnail
-                imgSrc={exampleImage2}
-                title="Some title"
-                subtitle="Some subtitle"
+                imgSrc="https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+                title="Mieszkanie"
+                subtitle="Niesamowita transformacja"
                 href="/realizacje/dom"
               />
             </div>
             <div className="grid-item--center">
               <Thumbnail
-                imgSrc={exampleImage3}
-                title="Some title"
-                subtitle="Some subtitle"
+                imgSrc="https://images.unsplash.com/photo-1502005229762-cf1b2da7c5d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                title="Apartament"
+                subtitle="Kombinacja drewna z malowaną na czarno stalą. Wszystko oświetlone ciepłym, żółtym światłem"
                 href="/realizacje/dom"
               />
             </div>
             <div className="grid-item--top-right">
               <Thumbnail
-                imgSrc={exampleImage4}
-                title="Some title"
-                subtitle="Some subtitle"
+                imgSrc="https://images.unsplash.com/photo-1542290504-2dc9a5811cd4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                title="Mieszkanie"
+                subtitle="Klimat dawnych lat zamknięty w nowoczesnym wykonaniu"
                 href="/realizacje/dom"
               />
             </div>
             <div className="grid-item--bottom-right">
               <Thumbnail
-                imgSrc={exampleImage5}
-                title="Some title"
-                subtitle="Some subtitle"
+                imgSrc="https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&auto=format&fit=crop&w=1506&q=80"
+                title="Dom"
+                subtitle="Przemyślany w każdym calu"
                 href="/realizacje/dom"
               />
             </div>
