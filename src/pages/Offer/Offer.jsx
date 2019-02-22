e771ddda89b8f21dd7f307fbf2422917bed28386
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-22 10:33:59 +0100

    Import the IcoMoon icons set

--- src/pages/Offer/Offer.jsx
+++ src/pages/Offer/Offer.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
-const Offer = () => <main className="Offer">Offer</main>;
+const Offer = () => (
+  <main className="Offer">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          {/* Heading */}
+          <h1 className="display-3 display-md-1 mb-5 text-center">Oferta</h1>
+
+          {/* Temporary icons tests */}
+          <div className="row">
+            <div className="col-4">
+              <i className="icon-helmet" />
+            </div>
+            <div className="col-4">
+              <i className="icon-hammer" />
+            </div>
+            <div className="col-4">
+              <i className="icon-lamp" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+);
 
 export default Offer;
