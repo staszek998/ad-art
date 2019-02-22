770e903c2fc7755bcbb32d2b9fdf737ef7031a3b
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-22 11:01:31 +0100

    Fix the desktop columns layout

--- src/pages/Offer/Offer.jsx
+++ src/pages/Offer/Offer.jsx
@@ -12,17 +12,17 @@ const Offer = () => (
 
           {/* Temporary icons tests */}
           <div className="row">
-            <div className="col-12 col-md-6 col-md-4 text-center py-4">
+            <div className="col-12 col-md-6 col-lg-4 text-center py-4">
               <i className="icon-blueprint" />
               <h2 className="h6 mt-2">
                 Profesjonalne wykonanie projektu z wizualizacją wnętrza
               </h2>
             </div>
-            <div className="col-12 col-md-6 col-md-4 text-center py-4">
+            <div className="col-12 col-md-6 col-lg-4 text-center py-4">
               <i className="icon-paint-brush" />
               <h2 className="h6 mt-2">Dobór kolorystyki we wnętrzach</h2>
             </div>
-            <div className="col-12 col-md-6 col-md-4 text-center py-4">
+            <div className="col-12 col-md-6 col-lg-4 text-center py-4">
               <i className="icon-lamp" />
               <h2 className="h6 mt-2">
                 Dobór elementów wyposażenia
@@ -30,23 +30,23 @@ const Offer = () => (
                 <small>(meble, tkaniny, tapety, oświetlenie, dekory)</small>
               </h2>
             </div>
-            <div className="col-12 col-md-6 col-md-4 text-center py-4">
+            <div className="col-12 col-md-6 col-lg-4 text-center py-4">
               <i className="icon-bags" />
               <h2 className="h6 mt-2">
                 Pomoc w zakupie materiałów i elementów wykończeniowych
               </h2>
             </div>
-            <div className="col-12 col-md-6 col-md-4 text-center py-4">
+            <div className="col-12 col-md-6 col-lg-4 text-center py-4">
               <i className="icon-tools" />
               <h2 className="h6 mt-2">Pomoc w wyborze firmy wykonawczej</h2>
             </div>
-            <div className="col-12 col-md-6 col-md-4 text-center py-4">
+            <div className="col-12 col-md-6 col-lg-4 text-center py-4">
               <i className="icon-helmet" />
               <h2 className="h6 mt-2">
                 Nadzór nad projektem na wszystkich etapach jego realizacji
               </h2>
             </div>
-            <div className="col-12 col-md-12 col-md-4 text-center py-4">
+            <div className="col-12 col-md-12 text-center py-4">
               <i className="icon-hammer" />
               <h2 className="h6 mt-2">
                 Współpracujemy z plastykami i konserwatorami mebli
