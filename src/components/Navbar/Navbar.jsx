9a6ee14d7abba233e7adb8dfe9745e619d721597
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-22 13:44:40 +0100

    Add box-shadow when the navbar is expanded on mobile screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,65 +5,72 @@ import "./_Navbar.scss";
 
 import adArtLogotype from "../../assets/images/ad-art-logotype.png";
 
-const Navbar = () => (
-  <nav className="Navbar navbar navbar-expand-sm">
-    <div className="container align-items-baseline">
-      {/* Company name */}
-      <NavLink className="brand-name" to="/">
-        <img src={adArtLogotype} alt="Logotyp firmy AD-art" />
-        AD-art
-      </NavLink>
+const Navbar = () => {
+  let navbar = React.createRef();
+  let hamburger = React.createRef();
 
-      {/* Dropdown toggle button */}
-      <button
-        className="hamburger hamburger--spin-r navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbar-collapse"
-        aria-controls="navbar-collapse"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        onClick={event => {
-          event.currentTarget.classList.toggle("is-active");
-        }}
-      >
-        <span className="hamburger-box">
-          <span className="hamburger-inner" />
-        </span>
-      </button>
+  return (
+    <nav className="Navbar navbar navbar-expand-sm" ref={navbar}>
+      <div className="container align-items-baseline">
+        {/* Company name */}
+        <NavLink className="brand-name" to="/">
+          <img src={adArtLogotype} alt="Logotyp firmy AD-art" />
+          AD-art
+        </NavLink>
 
-      {/* Navbar links */}
-      <div className="collapse navbar-collapse" id="navbar-collapse">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <NavLink className="art-link-hover" to="/o-nas">
-              O NAS
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="art-link-hover" to="/realizacje">
-              REALIZACJE
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="art-link-hover" to="/projekty">
-              PROJEKTY
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="art-link-hover" to="/oferta">
-              OFERTA
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="art-link-hover" to="/kontakt">
-              KONTAKT
-            </NavLink>
-          </li>
-        </ul>
+        {/* Dropdown toggle button */}
+        <button
+          ref={hamburger}
+          className="hamburger hamburger--spin-r navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbar-collapse"
+          aria-controls="navbar-collapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={event => {
+            hamburger.current.classList.toggle("is-active");
+            navbar.current.classList.toggle("expanded");
+          }}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner" />
+          </span>
+        </button>
+
+        {/* Navbar links */}
+        <div className="collapse navbar-collapse" id="navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="art-link-hover" to="/o-nas">
+                O NAS
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="art-link-hover" to="/realizacje">
+                REALIZACJE
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="art-link-hover" to="/projekty">
+                PROJEKTY
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="art-link-hover" to="/oferta">
+                OFERTA
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="art-link-hover" to="/kontakt">
+                KONTAKT
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
