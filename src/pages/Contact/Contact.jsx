1724620186cd2801c0f09ed5d39e15ac95edd257
Stanisław Gregor <stanislaw.gregor@compania.com.pl>
2019-02-22 11:20:37 +0100

    Make the Facebook icon larger

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./_Contact.scss";
+
 const Contact = () => (
   <main className="Contact">
     <div className="container">
@@ -10,7 +12,7 @@ const Contact = () => (
 
           <div className="row">
             {/* Contact details */}
-            <div className="col-12 col-md-6 text-center">
+            <div className="col-12 col-md-6 text-center Contact__contact-details">
               <p>
                 Działamy na terenie kraju, głównie: Zielona&nbsp;Góra
                 i&nbsp;miasta okoliczne.
@@ -25,13 +27,23 @@ const Contact = () => (
                 <br />
                 tel. <a href="tel:+48660773793">660 773 793</a>
               </p>
-              <p>
-                <a href="mailto:adart.zielonagora@gmail.com">
-                  adart.zielonagora@gmail.com
-                </a>
-                <br />
-                <a href="http://adart.org.pl">adart.org.pl</a>
-              </p>
+
+              <div className="row">
+                <div className="col-12 text-center">
+                  <a
+                    className="d-block"
+                    href="mailto:adart.zielonagora@gmail.com"
+                  >
+                    adart.zielonagora@gmail.com
+                  </a>
+                  <a className="d-block" href="http://adart.org.pl">
+                    adart.org.pl
+                  </a>
+                  <a className="d-block mt-4" href="http://facebook.com">
+                    <i className="icon-facebook" />
+                  </a>
+                </div>
+              </div>
             </div>
 
             {/* Contact form */}
